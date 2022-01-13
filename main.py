c0fd5a9da43c483b5cefceb0e1b338f0b0cd4990
Gabriel Nwokolo <92697325+LordTsagE@users.noreply.github.com>
2022-01-13 16:31:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+w = float(weight)
+h = float(height)
+h2 = h*h
+BMI = w/h2
+BMI2 = int(BMI)
+print(BMI2)
+#Write your code below this line 👇
+
+
+
+
+
+
+
+
+
+
